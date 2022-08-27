1431240310a9112e1293b0c78d4c1486d76d093d
Scott Calafiore <scottcal@gmail.com>
2022-08-27 12:16:57 -0700

    more async and promises

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const superagent = require('superagent');
 const readFilePro = (file) => {
   return new Promise((resolve, reject) => {
-    console.log(`hello world ${file}`);
     fs.readFile(file, (err, data) => {
       if (err) reject('could not find file');
 
@@ -20,19 +19,62 @@ const writeFilePro = (file, data) => {
   });
 };
 
-readFilePro(`${__dirname}/dog.txt`)
-  .then((data) => {
-    console.log(`Breed: ${data}`);
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
-    console.log(res.body.message);
+const getDogPic = async () => {
+  try{
+  const data = await readFilePro(`${__dirname}/dog.txt`)
+  console.log(`Breed: ${data}`);
+  console.log('hello world')
 
-    return writeFilePro('dog-img.txt', res.body.message);
-  })
-  .then((string) => {
-    console.log(`saved to file ${string}`);
-  })
-  .catch((err) => {
-    console.log(`err.message: ${err}`);
-  });
+  const res1Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  const res2Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  const res3Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  const all = await Promise.all([res1Pro, res2Pro, res3Pro])
+  const imgs = all.map(el => el.body.message)
+  console.log(imgs);
+
+  const string = await writeFilePro('dog-img.txt', imgs.join('\n'));
+  console.log(`saved to file ${string}`);
+  } catch (err){
+
+  console.log(err)
+  throw(err)
+  }
+  return '2: Ready 😎'
+}
+
+(async() => {
+  try{
+    console.log('1: will get dog pics')
+    const x = await getDogPic()
+    console.log(x)
+    console.log('3: done getting dog pics')
+  }catch(err) {
+    console.log('ERROR 💥')
+  }
+})();
+
+
+// console.log('1: will get dog pics')
+// getDogPic().then(x =>{
+//   console.log(x)
+//   console.log('3: done getting dog pics')
+// } ).catch(err =>{
+//   console.log('ERROR 💥')
+  
+// })
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     console.log(`Breed: ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body.message);
+
+//     return writeFilePro('dog-img.txt', res.body.message);
+//   })
+//   .then((string) => {
+//     console.log(`saved to file ${string}`);
+//   })
+//   .catch((err) => {
+//     console.log(`err.message: ${err}`);
+//   });
